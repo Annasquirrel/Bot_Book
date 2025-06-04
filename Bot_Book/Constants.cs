7e780341f535138716c8e099f3a4309c9a7e2d59
Annasquirrel <annasquirrel132@gmail.com>
2025-06-04 22:39:51 +0300

    Change const

--- Bot_Book/Constants.cs
+++ Bot_Book/Constants.cs
@@ -8,7 +8,7 @@ namespace Bot_Book
 {
     public class Constants
     {
-        public static string BaseUrl = "https://localhost:7018/api/Book/"; 
+        public static string BaseUrl = "https://localhost:7121/api/Book/"; 
         public static string ApiKey = "AIzaSyCMLOlwSCNi2t9E_n1xDbpr1qs6-Nf7zWs";
     }
 }
